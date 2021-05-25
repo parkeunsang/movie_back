be0dd3e6d56d39ee3fa27764121fca902c48758a
parkeunsang <dislive@naver.com>
2021-05-25 15:52:49 +0900

    추천 결과

--- recommends/models.py
+++ recommends/models.py
@@ -4,5 +4,12 @@ from movies.models import Movie
 # Create your models here.
 class Keyword(models.Model):
     word = models.CharField(max_length=100)
-    movies = models.ManyToManyField(Movie, related_name='recommend_words')
+    movies = models.ManyToManyField(Movie, related_name='recommend_words', through='KM')
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='keywords')
+
+
+class KM(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    word = models.ForeignKey(Keyword, on_delete=models.CASCADE)
+    score = models.FloatField()
+    
