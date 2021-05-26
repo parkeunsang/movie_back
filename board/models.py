0bb728b95591dcc84056f6f87b26130c79224d94
parkeunsang <dislive@naver.com>
2021-05-27 04:08:28 +0900

    글이랑 댓글 수정

--- board/models.py
+++ board/models.py
@@ -16,3 +16,5 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 외래키
     review = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
