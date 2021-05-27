2241f38db17adfffb663b5baffc4301e10f5f234
parkeunsang <dislive@naver.com>
2021-05-27 13:44:20 +0900

    잡다한것들 수정

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,8 @@ app_name = 'board'
 
 urlpatterns = [
     path('articles/', views.article_list_or_create),
+    path('articles/check/<int:article_pk>/', views.article_check),
+    path('comment/check/<int:comment_pk>/', views.comment_check),
     # path('articles/', views.create_article),
     path('articles/<int:article_pk>/', views.article_detail),
     path('comment/<int:pk>/', views.comment_list_or_create_or_delete),
